9ac7f31b919263408ee4e02e29fc65884ad03296
Jack YC Lin <jackyc.lin@adlinktech.com>
2023-11-02 14:32:52 +0800

    add user menu & specify line_index

--- get_excel_data.py
+++ get_excel_data.py
@@ -5,9 +5,9 @@ excel第一行必須是料號
 '''
 import pandas as pd
 
-def excel_to_list(path: str, sheet_name: str):
+def excel_to_list(path: str, sheet_name: str = None, line_index: int = 0):
     '''料號轉一維list'''
-    df = pd.read_excel("data_sheets/"+path, sheet_name=sheet_name).iloc[:,[0]]
+    df = pd.read_excel("data_sheets/"+path, sheet_name=sheet_name).iloc[:,[line_index]]
     object_list = df.values.tolist()
     flatten_list = lambda l : [flat for tmp in l for flat in flatten_list(tmp)] if type(l) == list else [l]
 
